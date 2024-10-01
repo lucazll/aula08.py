d28d2147d5dd237f5bec7d6ae83199b23f6532fb
Lucas <>
2024-10-01 09:09:30 -0300

    aula12

---
+++ atv_aula12.py
@@ -0,0 +1,9 @@
+def notas(nota1, nota2, nota3):
+    media = (nota1 + nota2 + nota3) /3
+    return media
+
+nota1 = int(input('DIGITE A NOTA 1: '))
+nota2 = int(input('DIGITE A NOTA 2: '))
+nota3 = int(input('DIGITE A NOTA 3: '))
+
+notas(nota1, nota2, nota3)
